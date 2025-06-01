82846bfe65b8e29ba9846c01c719c1b3dce47259
Shreya Sikder <123325349+Shreya-Sikder@users.noreply.github.com>
2025-06-02 00:19:14 +0600

    Add files via upload

---
+++ BASICS OF PHP/loop.php
@@ -0,0 +1,23 @@
+<?php
+// echo "For Loop Example<br>";
+/*
+for ($i = 0; $i < 10; $i+=2) {
+    echo "Current number is: $i<br>";
+}
+*/
+// echo "while Loop Example<br>";
+$i = 1;
+while ($i < 10) {
+    echo "Current number is: $i<br>";
+    $i += 2;
+}
+
+
+
+
+
+
+
+
+
+?>
