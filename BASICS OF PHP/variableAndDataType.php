2387a0df230ce7a62da139f39798a7f177f12f58
Shreya Sikder <123325349+Shreya-Sikder@users.noreply.github.com>
2025-06-02 00:20:06 +0600

    Add files via upload

---
+++ BASICS OF PHP/variableAndDataType.php
@@ -0,0 +1,17 @@
+<?php
+$firstname = "John";//$=variable sign
+$lastname = "Doe";
+echo $firstname;
+$firstname = "meow";//"---"= string
+echo "<br>";
+echo $firstname;
+echo "<br>";
+$a=10.99;//float
+$b=20; //integer
+echo $a + $b;
+echo "<br>";
+$booleanCheck = true; //boolean
+
+$food = null;
+
+?>
