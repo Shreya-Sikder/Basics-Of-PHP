40173025a5db5ded0c0eb8085b70c736f2dee40f
Shreya Sikder <123325349+Shreya-Sikder@users.noreply.github.com>
2025-06-02 00:19:29 +0600

    Add files via upload

---
+++ BASICS OF PHP/operators.php
@@ -0,0 +1,33 @@
+<?php
+$a=10;
+$b=20;
+// "=" operator is used to assign values to variables
+$addition = $a + $b;
+echo $addition;
+echo "<br>";
+$subtraction = $a - $b;
+echo $subtraction;
+echo "<br>";
+$multiplication = $a * $b;
+echo $multiplication;
+echo "<br>";
+$division = $a / $b;
+echo $division;
+echo "<br>";
+$modulus = $a % $b;
+echo $modulus;
+echo "<br>";
+$exponentiation = ($a+2) ** $b;
+echo $exponentiation;
+echo "<br>";
+echo "<br>";
+echo "Assignment Operators<br>";
+$x = 10;
+$x += 5; // $x = $x + 5
+echo $x;
+echo "<br>";
+var_dump($b);// datatype(value) print hoy
+
+
+
+?>
